c390ec661eb6a9f532979281a34a031d0684c31e
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2022-01-17 18:33:40 -0600

    Initial commit

---
+++ bias_validation.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python
+import argparse
+import logging
+import coloredlogs
+import tomli as tml
+from pathlib import Path
+import difflib
+import shutil
+import subprocess
+import sys
+
+# Relative imports
+from files.scripts.confidence_intervals import calculate_confidence_interval, analyse_confidence_interval
+from files.scripts.contour import get_chain_paths, load_chains, plot_chains
+
+config = {
+        'logfile': 'log.txt',
+        'base': '/project2/rkessler/SURVEYS/DES/USERS/parmstrong/inputs/bias_validation/',
+        'clean': True
+        }
+
+
+def ensure_list(l):
+    if isinstance(l, list):
+        return l
+    return [l]
+
+# Logging stuff just stolen from pippin
+
+def setup_logging(logging_filename, verbose):
+
+    level = logging.DEBUG if verbose else logging.INFO
+
+    fmt = "[%(levelname)8s |%(filename)21s:%(lineno)3d]   %(message)s" if verbose else "%(message)s"
+    handlers = [logging.StreamHandler()]
+    handlers[0].setLevel(level)
+    handlers.append(logging.FileHandler(logging_filename, mode="w"))
+    handlers[-1].setLevel(logging.DEBUG)
+    logging.basicConfig(level=level, format=fmt, handlers=handlers)
+
+    coloredlogs.install(
+        level=level,
+        fmt=fmt,
+        reconfigure=True,
+        level_styles=coloredlogs.parse_encoded_styles("debug=8;info=green;warning=yellow;error=red,bold;critical=red,inverse"),
+    )
+    logging.getLogger("matplotlib").setLevel(logging.ERROR)
+
+
+def full_run_setup(full_runs, config, logger):
+    if len(full_runs) == 0:
+        logger.error("No full runthroughs specified, quitting")
+        return None
+    full_run_paths = []
+    for full_run in full_runs:
+        logger.info(f"Setting up {full_run}")
+        # Grab parameters from input file
+        Om = full_runs[full_run][0]['OMEGA_MATTER']
+        w0 = full_runs[full_run][0]['W0_LAMBDA']
+        name = f"BV_FR_{full_run}.yml"
+        full_run_master = config.get("input_files") / "full_run.yml"
+        # Edit the master yml file
+        with open(full_run_master, 'r') as f:
+            tmp = f.read()
+        tmp =tmp.replace("$W0_LAMBDA", str(w0))
+        tmp = tmp.replace("$OMEGA_MATTER", str(Om))
+        # Write out to pippin_inputs
+        prepend = f"#Generated via:\n#[[ full_run.{full_run} ]]\n#OMEGA_MATTER = {Om}\n#W0_LAMBDA = {w0}\n\n"
+        tmp = prepend + tmp
+        full_run_final = config.get("pippin_inputs") / name
+        skip = False # Whether or not to skip running pippin
+        if full_run_final.exists():
+            logger.info(f"Found another input file called {name}, comparing them now")
+            with open(full_run_final, 'r') as f:
+                tmp2 = f.read()
+            diffs = list(difflib.unified_diff(tmp.split('\n'), tmp2.split('\n'), fromfile="new_input", tofile="old_input", lineterm=''))
+            if len(diffs) == 0:
+                logger.info("No differences found, will not rerun")
+                skip = True
+            else:
+                logger.warning(f"Found the following differences:")
+                for line in diffs:
+                    logger.warning(line)
+                logger.warning("If you continue, you will overwrite previously defined inputs. Would you like to continue? (y/n)")
+                response = input()
+                if response == 'y':
+                    logger.warning("You have chosen to continue, overwriting previous input file.") 
+                else:
+                    logger.warning("Quitting")
+                    return None 
+        if not full_run_final.exists():
+            full_run_final.touch(exist_ok=True)
+        with open(full_run_final, 'w') as f:
+            f.write(tmp)
+        full_run_paths.append((full_run_final, skip, Om, w0))
+    return full_run_paths
+
+def validation_setup(validations, config, logger):
+    if len(validations) == 0:
+        logger.error("No validations specified, quitting")
+        return None
+    validation_paths = []
+    for validation in validations:
+        logger.info(f"Setting up {validation}")
+        # Grab parameters from input file
+        ompri = bool(validations[validation][0]["ompri"])
+        Om = validations[validation][0]["OMEGA_MATTER"]
+        w0 = validations[validation][0]["W0_LAMBDA"]
+        name = f"BV_V_{validation}.yml"
+        validation_master = config.get("input_files") / "validation.yml"
+        # Edit the master yml file
+        with open(validation_master, 'r') as f:
+            tmp = f.read()
+        if ompri:
+            tmp = tmp.replace("$BCOR_BASE", "SALT2mu.input")
+        else:
+            tmp = tmp.replace("$BCOR_BASE", "SALT2mu_no_ompri.input")
+        DESSIM = ""
+        BCOR = ""
+        ompri_str = 'ompri' if ompri else 'no_ompri'
+        for m in ensure_list(Om):
+            m_str = str(m).replace('-','n').replace('.','_')
+            for w in ensure_list(w0):
+                w_str = str(w).replace('-','n').replace('.','_')
+                s = f"  DESSIM_w_{w_str}_om_{m_str}:\n    <<: *sim_config\n    GLOBAL:\n      <<: *sim_global\n      OMEGA_MATTER: {str(float(m))}\n      W0_LAMBDA: {str(float(w))}\n      OMEGA_LAMBDA: {str(1 - float(m))}\n\n"
+                DESSIM += s
+                s = f"  BCOR_{ompri_str}_w_{w_str}_om_{m_str}:\n      <<: *bcor_config\n      DATA: [DESFITSYS_DESSIM_w_{w_str}_om_{m_str}]\n\n"
+                BCOR += s
+        tmp = tmp.replace('$DESSIM', DESSIM)
+        tmp = tmp.replace('$BCOR', BCOR)
+        # Write out to pippin_inputs
+        prepend = f"#Generated via:\n#[[ validation.{validation} ]]\n#ompri = {ompri}\n#OMEGA_MATTER = {Om}\n#W0_LAMBDA = {w0}\n\n"
+        tmp = prepend + tmp
+        validation_final = config.get("pippin_inputs") / name
+        skip = False # Whether or not to skip running pippin
+        if validation_final.exists():
+            logger.info(f"Found another input file called {name}, comparing them now")
+            with open(validation_final, 'r') as f:
+                tmp2 = f.read()
+            diffs = list(difflib.unified_diff(tmp.split('\n'), tmp2.split('\n'), fromfile="new_input", tofile="old_input", lineterm=''))
+            if len(diffs) == 0:
+                logger.info("No differences found, will not rerun")
+                skip = True
+            else:
+                logger.warning(f"Found the following differences:")
+                for line in diffs:
+                    logger.warning(line)
+                logger.warning("If you continue, you will overwrite previously defined inputs. Would you like to continue? (y/n)")
+                response = input()
+                if response == 'y':
+                    logger.warning("You have chosen to continue, overwriting previous input file.")
+                else:
+                    logger.warning("Quitting.")
+                    return None
+        if not validation_final.exists():
+            validation_final.touch(exist_ok=True)
+        with open(validation_final, 'w') as f:
+            f.write(tmp)
+        validation_paths.append((validation_final, skip))
+    return validation_paths
+    
+def get_args():
+    # Set up command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("toml", help="the name of the toml config file to run.", type=str)
+    parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
+    parser.add_argument("-n", "--no_skip", help="Whether to rerun pippin jobs regardless of whether a change has been made", action="store_true")
+
+    args = parser.parse_args()
+    return args
+
+def run():
+    args = get_args()
+    toml_path = Path(args.toml)
+    with open(toml_path, 'rb') as f:
+        toml = tml.load(f)
+    # Setup paths
+    config["base"] = Path(config["base"])
+    config["files"] = config["base"] / "files"
+    config["input_files"] = config["files"] / "input_files"
+    config["inputs"] = config["base"] / "inputs"
+    config["pippin_inputs"] = config["inputs"] / "Pippin"
+    config["outputs"] = config["base"]/ "outputs"
+    config["output"] = config["outputs"] / toml_path.stem # Add config file name to output
+    output = config["output"]
+    if not output.exists():
+        output.mkdir(parents=True, exist_ok=True)
+    logfile = output / config.get("logfile")
+    if not logfile.exists():
+        logfile.touch(exist_ok=True)
+    setup_logging(logfile, args.verbose)
+    logger = logging.getLogger("bias_validation") 
+    logger.info(f"Config complete, output folder set to {output}, logging to {logfile})")
+    logger.info("Preparing for full run stage")
+    # Start of run
+    full_run_paths = full_run_setup(toml.get("full_run", []), config, logger)
+    if full_run_paths is None:
+        logger.error("Error occured during full runthrough setup, quitting.")
+        return None
+    logger.info(f"Full runthrough setup complete, found {len(full_run_paths)} runthroughs")
+    logger.debug(f"Full runthrough paths: {full_run_paths}")
+    logger.info("Running the full runthroughs now")
+    for i, (path, skip, _, _) in enumerate(full_run_paths):
+        if skip and not args.no_skip:
+            logger.info(f"Skipping {i + 1}")
+            continue
+        logger.info(f"({i + 1}/{len(full_run_paths)})")
+        cmd = ["pippin.sh", str(path)]
+        logger.debug(f"Running {cmd}")
+        cmd = subprocess.run(cmd)
+        logger.debug(f"Completed with exit code {cmd.returncode}")
+    logger.info("Completed full runthroughs")
+    # Start of validation
+    logger.info("Setting up validation jobs")
+    validation_paths = validation_setup(toml.get("validation", []), config, logger)
+    if validation_paths is None:
+        logger.error("Error occured during validation jobs setup, quitting")
+        return None
+    logger.info(f"Validation setup complete, found {len(validation_paths)} jobs")
+    logger.debug(f"Validation jobs: {validation_paths}")
+    logger.info("Running validation jobs now")
+    for i, (path, skip) in enumerate(validation_paths):
+        if skip and not args.no_skip:
+            logger.info(f"Skipping {i + 1}")
+            continue
+        logger.info(f"({i + 1}/{len(validation_paths)})")
+        cmd = ["pippin.sh", str(path)]
+        logger.debug(f"Running {cmd}")
+        cmd = subprocess.run(cmd)
+        logger.debug(f"Compeleted with exit code {cmd.returncode}")
+    logger.info("Completed validation jobs")
+    # Calculating confidence intervals
+    logger.info("Calculating confidence intervals")
+    ci_config = toml["confidence_interval"]
+    do_plot = ci_config.get("plot", False) 
+    plot_path = output if do_plot else None
+    ci = {}
+    for (path, skip) in validation_paths:
+        name = path.stem.upper()    
+        ci[name] = {}
+        ci_stats = calculate_confidence_interval(name, logger)
+        for w0 in ensure_list(ci_config["w0"]):
+            for Om in ensure_list(ci_config["Om"]):
+                wl, wr, oml, omr = analyse_confidence_interval(*ci_stats, name, w0, Om, logger, plot_path)
+                ci[name]["w0l"] = wl
+                ci[name]["w0r"] = wr
+                ci[name]["oml"] = oml
+                ci[name]["omr"] = omr
+    logger.debug(f"Confidence intervals: {ci}")
+    logger.info("Finished calculating confidence intervals")
+    # Creating contours 
+    logger.info("Creating full runthrough contours")
+    for (path, skip, Om, w0) in full_run_paths:
+        logger.info(f"Creating contour for {path.stem.upper()}")
+        chain_paths = get_chain_paths(path.stem.upper(), logger)
+        chains = load_chains(chain_paths, masks=[['(SN)'], ['(SN + PRIOR)']], logger=logger) 
+        plot_chains(output, chains, w0=w0, Om=Om, confidence_interval=ci, logger=logger)
+    logger.info(f"All done! See {output} for output")
+
+
+
+if __name__ == "__main__":
+    run()
---
+++ files/scripts/contour.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use("Agg")
+from chainconsumer import ChainConsumer
+import numpy as np
+from pathlib import Path
+import argparse
+import logging
+from tqdm import tqdm
+import io
+
+def get_chain_paths(name, logger=None):
+    if logger is not None:
+        logger.info(f"Reading in chains for {name}")
+    else:
+        print(f"Reading in chains for {name}")        
+    path = Path(f"/scratch/midway2/rkessler/PIPPIN_OUTPUT/{name}/9_ANALYSE/ALL_OMW")
+    chain_paths = [p for p in path.iterdir() if p.is_file() and p.suffixes == ['.csv', '.gz']]
+    if logger is not None:
+        logger.info(f"Found {len(chain_paths)} chains")
+    else:
+        print(f"Found {len(chain_paths)} chains")
+    return chain_paths
+
+def load_chains(chain_paths, masks=None, num_cores=8, logger=None):
+    if masks is None:
+        masks = [""]
+    else:
+        masks = [m[0] for m in masks]
+    weights = {m: [] for m in masks}
+    likelihoods = {m: [] for m in masks}
+    Om = {m: [] for m in masks}
+    w0 = {m: [] for m in masks}
+    if logger is not None:
+        logger.info(f"Loading chains into chainconsumer")
+    else:
+        print(f"Loading chains into chainconsumer")
+    def _load_chain(path):
+        return (path, np.transpose(np.loadtxt(path, dtype=float, skiprows=1, delimiter=","))) # Load in. remove header, transpose
+    shared_data = [_load_chain(path) for path in tqdm(chain_paths)]
+    for (path, data) in shared_data:
+        for key in masks:
+            if key in path.name:
+                weights[key].append(data[0])
+                likelihoods[key].append(data[1])
+                Om[key].append(data[2])
+                w0[key].append(data[3])
+    chains = {}
+    for key in masks:
+        weights[key] = np.concatenate(weights[key])
+        likelihoods[key] = np.concatenate(likelihoods[key])
+        Om[key] = np.concatenate(Om[key])
+        w0[key] = np.concatenate(w0[key])
+        c = ChainConsumer()
+        c.add_chain([Om[key], w0[key]], parameters = ["$\Omega_{M}$", "$\omega_{0}$"], weights=weights[key], power=2)
+        c.configure(statistics="cumulative")
+        if logger is not None:
+            logger.info(f"Finished combining chains for mask {key}:\n{c.analysis.get_summary()}")
+        else:
+            print(f"Finished combining chains for mask {key}:\n{c.analysis.get_summary()}")
+        chains[key] = c
+    return chains
+
+def plot_chains(plot, chains, w0=-1, Om=0.3, confidence_interval=None, logger=None):
+    if confidence_interval is None:
+        confidence_interval = []
+    for key in chains.keys():
+        c = chains[key]
+        plt.ioff()
+        fig_width_pt = 4 * 240
+        inches_pt = 1.0 / 72.27
+        fig_width = fig_width_pt * inches_pt
+        fig = c.plotter.plot(display=False, truth=[w0, Om], figsize=(fig_width, fig_width / np.sqrt(2)))
+        ax = fig.get_axes()
+        for ci in confidence_interval.values():
+            ax[2].plot([ci["oml"], ci["omr"]], [ci["w0l"], ci["w0r"]], c='black')
+        plt.savefig(Path(plot) / f"{key}.png")
+        plt.clf()
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", help="The name of the pippin output folder", type=str)
+    parser.add_argument("-p", "--plot", help="Directory to store plots", type=str, default=None)
+    parser.add_argument("-w", "--w0", help="w0 value used in simulation", type=float, default=-1.0)
+    parser.add_argument("-o", "--Om", help="Om value used in simulation", type=float, default=0.3)
+    parser.add_argument("-c", "--confidence_interval", help="Confidence interval. Can be added multiple times, syntax is -c OmegaMatterBottom OmegaMatterTop W0Left W0Right", action='append', nargs='+', type=float, default=None)
+    parser.add_argument("-m", "--masks", help="Mask pippin analysis output. Can be added multiple times. Partial match", action='append', nargs='+', type=str, default=None)
+    parser.add_argument("-n", "--num_cores", help="How many cores to use. Warning, using to many can get you kicked off midway!", type=int, default=8)
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = get_args()
+    name = args.name
+    plot = args.plot
+    Om = args.Om
+    w0 = args.w0
+    confidence_interval = {}
+    ci = args.confidence_interval
+    for i, c in enumerate(ci):
+        confidence_interval[i] = {'oml': c[0], 'omr': c[1], 'w0l': c[2], 'w0r': c[3]}
+    masks = args.masks
+    num_cores = args.num_cores
+    chain_paths = get_chain_paths(name)
+    chains = load_chains(chain_paths, masks=masks, num_cores=num_cores)
+    if plot is not None:
+        plot_chains(plot, chains, w0=w0, Om=Om, confidence_interval=confidence_interval)
+
+if __name__ == "__main__":
+    main()
